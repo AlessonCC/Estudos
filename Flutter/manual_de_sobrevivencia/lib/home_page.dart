cb02592902c4ea54714b76d7cd091a498d590c99
alesson <alessoncarlos20@gmail.com>
2022-04-30 19:15:07 -0300

    App Contador

---
+++ Flutter/manual_de_sobrevivencia/lib/home_page.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() {
+    return HomePageState();
+    throw UnimplementedError();
+  }
+}
+
+class HomePageState extends State<HomePage> {
+  int count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+      ),
+      body: Center(
+          child: GestureDetector(
+        child: Text(
+          'Contador: $count',
+          style: TextStyle(fontSize: 30),
+        ),
+        onTap: () {
+          setState(() {
+            count++;
+          });
+        },
+      )),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          setState(() {
+            count++;
+          });
+        },
+      ),
+    );
+  }
+}
--- Flutter/manual_de_sobrevivencia/lib/main.dart
+++ Flutter/manual_de_sobrevivencia/lib/main.dart
@@ -1,49 +1,7 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'app_widget.dart';
+
 void main() {
-  runApp(AppWidget(
-    title: "Tela Principal",
-  ));
-}
-
-class AppWidget extends StatelessWidget {
-  final String title;
-
-  const AppWidget({Key? key, required this.title}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.red),
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  @override
-  State<HomePage> createState() {
-    return HomePageState();
-    throw UnimplementedError();
-  }
-}
-
-class HomePageState extends State<HomePage> {
-  int count = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-          child: GestureDetector(
-        child: Text('Contador: $count'),
-        onTap: () {
-          setState(() {
-            count++;
-          });
-        },
-      )),
-    );
-  }
+  runApp(AppWidget());
 }
