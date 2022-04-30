3eb4dc34edde355c59bec5e16d4c151d8e9a1c96
alesson <alessoncarlos20@gmail.com>
2022-04-30 20:16:24 -0300

    Modo Dark melhorado

--- Flutter/manual_de_sobrevivencia/lib/home_page.dart
+++ Flutter/manual_de_sobrevivencia/lib/home_page.dart
@@ -19,15 +19,11 @@ class HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Home Page'),
+        actions: [CustomSwitch()],
       ),
       body: Align(
         alignment: Alignment.topLeft,
-        child: Switch(
-          value: AppController.instance.isDarkTheme,
-          onChanged: (value) {
-            AppController.instance.changeTheme();
-          },
-        ),
+        child: CustomSwitch(),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
@@ -37,6 +33,18 @@ class HomePageState extends State<HomePage> {
           });
         },
       ),
+    );
+  }
+}
+
+class CustomSwitch extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Switch(
+      value: AppController.instance.isDarkTheme,
+      onChanged: (value) {
+        AppController.instance.changeTheme();
+      },
     );
   }
 }
