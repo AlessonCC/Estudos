80e36feb1516e79165895d0be8c660ee2ea6a684
alesson <alessoncarlos20@gmail.com>
2022-05-01 12:07:30 -0300

    Navegacao entre telas, adicao de imagens, modificacao do assets

--- Flutter/manual_de_sobrevivencia/lib/login_page.dart
+++ Flutter/manual_de_sobrevivencia/lib/login_page.dart
@@ -1,6 +1,10 @@
 // ignore_for_file: deprecated_member_use
 
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+
+import 'home_page.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -10,34 +14,54 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  String email = '';
+  String password = '';
+
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: SizedBox(
-        width: double.infinity,
-        height: double.infinity,
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const TextField(
+              Container(
+                width: 200,
+                height: 200,
+                child: Image.asset('assets/images/IfsLogo.png'),
+              ),
+              Container(height: 50),
+              TextField(
+                onChanged: (text) {
+                  email = text;
+                },
                 keyboardType: TextInputType.emailAddress,
                 decoration: InputDecoration(
                   labelText: 'Login',
                   border: OutlineInputBorder(),
                 ),
               ),
-              const SizedBox(height: 10),
-              const TextField(
+              SizedBox(height: 10),
+              TextField(
+                  onChanged: (text) {
+                    password = text;
+                  },
                   obscureText: true,
                   decoration: InputDecoration(
                     border: OutlineInputBorder(),
                     labelText: 'Password',
                   )),
-              const SizedBox(height: 15),
+              SizedBox(height: 15),
               RaisedButton(
-                onPressed: () {},
+                onPressed: () {
+                  if (email == 'alesson' && password == '123') {
+                    Navigator.of(context).pushReplacementNamed('/home');
+                  } else {
+                    print("Login ou senha incorreto");
+                  }
+                },
                 child: Text('Entrar'),
               )
             ],
