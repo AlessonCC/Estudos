858e21fd5c1147b9831b9927cbad48b92170a800
alesson <alessoncarlos20@gmail.com>
2022-05-23 15:27:02 -0300

    Correção de erros

--- Flutter/manual_de_sobrevivencia/lib/app_widget.dart
+++ Flutter/manual_de_sobrevivencia/lib/app_widget.dart
@@ -24,6 +24,7 @@ class AppWidget extends StatelessWidget {
             '/': (context) => const LoginPage(),
             '/home': (context) => HomePage(),
           },
+          debugShowCheckedModeBanner: false,
         );
       },
     );
